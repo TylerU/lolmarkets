18d831dce73db28c46030e520b88b9ade839dbba
TylerU <dinoentrails@gmail.com>
2017-01-01 15:35:20 -0600

    Add some filters for marketUser

--- api/services/market-user/market-user-filters.js
+++ api/services/market-user/market-user-filters.js
@@ -1,8 +1,22 @@
+const filters = require('../../filters');
+const schema = require('./market-user-schema');
+const outProperties = schema.outProperties;
+
+function ensureBelongsToUser(data, connection, hook) {
+  if (connection.user && `${connection.user.id}` === `${data.user}`) {
+    return data;
+  }
+  return false;
+}
 
 exports.filters = {
-  all: [() => false],
+  all: [
+    filters.ensureAuthenticated(),
+    ensureBelongsToUser,
+    filters.pluckFilter(outProperties),
+  ],
   created: [],
   updated: [],
   patched: [],
-  removed: [],
+  removed: [() => false],
 };
